6ab6fc86ce0747f01481d5e1364ec3d489de84f8
deepsd13 <shah98@sheridancollege.ca>
2021-04-05 12:48:03 -0400

    Added Auth Token

--- eventProcessing.py
+++ eventProcessing.py
@@ -6,14 +6,22 @@ import requests
 
 EMULATE_HX711=False
 WEIGHT_NEGLEGANCE = 10
+ANALYZE_NEGLEGANCE = 10
 REFERENCE_UNIT = -26.4655172414
+# DEEP's 
+#DEVICE_TOKEN = "ex6VPaBdS1O71wNWqJu-St:APA91bFJx5t4kLNyfcqV9nN_TawoDau4ETUPWl3_wYJBpv-8dyG9cc8nYHP16WO6cwoa1LxNX7DM_prAgNg9fDdpjxgClCFrvPtuMpaAaCJ2NeCnUIBS9_Fb12xHylalMNb1maxI5Ifh"
 
-
+# DAVID's
+DEVICE_TOKEN = "eZLm6ggkTzqElKhn8vkvmq:APA91bEfXYGqKSVHk07oFmHsictLYxc4XCpENvhIjWtb-c9r87FSouRxzJYxp_qCnI5NJvQUzG4OmeWt4xgIpIlUTxIPE6hD_9wxhzP5XTGWfC3za0GbS2qcX5BGXo3ohpMO6bn5w0IL"
 #pin numbers for hx711
 dout = 5
 pd_sck = 6
 
-currentWeight = 0
+global currentWeight
+global buffer_values
+global confidence
+
+buffer_values = []
 
 if not EMULATE_HX711:
     import RPi.GPIO as GPIO
@@ -24,36 +32,68 @@ else:
 def cleanAndExit():
     print("Cleaning...")
 
-    if not EMULATE_HX711:
+    if not EMULATE_HX711: 
         GPIO.cleanup()
         
     print("Bye!")
     sys.exit()
+    
+def getAccessToken(url, user, password):
+    # token = "access toekn" {get from backend}
+    body={
+        "userName" : user,
+        "password" : password
+    }
+    r = requests.post(url, json=body)
+    print(r.json()['token'])
+    return r.json()['token']
 
-def sendPackagePlacedNotification(url):
-    r = requests.get(url)
-    data = r.json()
-    print(data[0].imageTitle)
+def sendNotification(url,title, message, token):
+    notificationData={
+        "Title":title,
+        "Message":message,
+        "DeviceToken":token
+    }
+    
+     # headers={'Authorization':"bearer "+ token}
+    token = getAccessToken("http://csr.fast.sheridanc.on.ca:50271/api/Authenticate/Login", "admin", "admin")
+    headers = {'Authorization':"bearer "+ token}
+    
+    r = requests.post(url, json=notificationData, headers = headers)
+    
+    print(r.text)
+    print(title)
 
-def sendPackagePickedUpNotification(url):
-    r = requests.get(url)
-    data = r.json()
-    print(data[0].imageName)
-
+def analyze(arr):
+    if max(arr)- min(arr) < ANALYZE_NEGLEGANCE:
+        return True
+    return False
 
 def weightProcessing(hx):
+    currentWeight = 0
     while True:
         try:
             val = hx.get_weight(5)
-            if val > currentWeight + WEIGHT_NEGLEGANCE:
-                sendPackagePlacedNotification("https://api-image-repository.herokuapp.com/api/image")
-            elif val < currentWeight - WEIGHT_NEGLEGANCE:
-                #alarmCheck()
-                sendPackagePickedUpNotification("https://api-image-repository.herokuapp.com/api/image")
-            print(val)
-            hx.power_down()
-            hx.power_up()
-            time.sleep(0.1)
+            print(val , currentWeight)
+            
+            if(len(buffer_values) == 5):
+                confidence = analyze(buffer_values)
+                
+                if confidence and val > currentWeight + WEIGHT_NEGLEGANCE:
+                    sendNotification("http://csr.fast.sheridanc.on.ca:50271/api/Notification","Package placed","Your Package has been Placed!)",DEVICE_TOKEN)
+                    currentWeight = sum(buffer_values) / len(buffer_values)
+                elif confidence and val < currentWeight - WEIGHT_NEGLEGANCE:
+                    #alarmCheck()
+                    sendNotification("http://csr.fast.sheridanc.on.ca:50271/api/Notification","Package picked up","Your package has been picked up!",DEVICE_TOKEN)
+                    currentWeight = val
+                hx.power_down()
+                hx.power_up()
+                time.sleep(0.1)
+                del buffer_values[:]
+                    
+            else:
+                buffer_values.append(val)
+                #print(len(buffer_values))
 
         except (KeyboardInterrupt, SystemExit):
             cleanAndExit()
