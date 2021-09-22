3628be86b9771a977b4ce4e431600e8da3927a0d
rajput0@sheridancollege.ca <rajput0@sheridancollege.ca>
2021-09-21 21:14:41 -0400

    Updated notifications api call and synced pi with backend

--- eventProcessing.py
+++ eventProcessing.py
@@ -3,13 +3,17 @@
 import time
 import sys
 import requests
+import threading
 
 EMULATE_HX711=False
 WEIGHT_NEGLEGANCE = 10
 ANALYZE_NEGLEGANCE = 10
 REFERENCE_UNIT = -26.4655172414
+# DEEP's 
 DEVICE_TOKEN = "ex6VPaBdS1O71wNWqJu-St:APA91bFJx5t4kLNyfcqV9nN_TawoDau4ETUPWl3_wYJBpv-8dyG9cc8nYHP16WO6cwoa1LxNX7DM_prAgNg9fDdpjxgClCFrvPtuMpaAaCJ2NeCnUIBS9_Fb12xHylalMNb1maxI5Ifh"
 
+# DAVID's
+#DEVICE_TOKEN = "eZLm6ggkTzqElKhn8vkvmq:APA91bEfXYGqKSVHk07oFmHsictLYxc4XCpENvhIjWtb-c9r87FSouRxzJYxp_qCnI5NJvQUzG4OmeWt4xgIpIlUTxIPE6hD_9wxhzP5XTGWfC3za0GbS2qcX5BGXo3ohpMO6bn5w0IL"
 #pin numbers for hx711
 dout = 5
 pd_sck = 6
@@ -34,38 +38,56 @@ def cleanAndExit():
         
     print("Bye!")
     sys.exit()
-
-def sendNotification(url,title, message, token):
-    notificationData={
-        "Title":title,
-        "Message":message,
-        "DeviceToken":token
+    
+def getAccessToken(url, user, password):
+    # token = "access toekn" {get from backend}
+    body={
+        "userName" : user,
+        "password" : password
     }
-    r = requests.post(url, json = notificationData)
-    print(r.text)
-    print(title)
+    r = requests.post(url, json=body)
+    return r.json()['token']
+
+def sendNotification(url):
+
+     # headers={'Authorization':"bearer "+ token}
+    token = getAccessToken("http://csr.fast.sheridanc.on.ca:50271/api/Authenticate/Login", "admin", "admin")
+    headers = {'Authorization':"bearer "+ token}
+    
+    r = requests.post(url, headers = headers)
+    
+    print(r.status_code)
 
 def analyze(arr):
     if max(arr)- min(arr) < ANALYZE_NEGLEGANCE:
         return True
     return False
 
+def sync():
+    threading.Timer(15.0,sync).start()
+    
+    token = getAccessToken("http://csr.fast.sheridanc.on.ca:50271/api/Authenticate/Login", "admin", "admin")
+    headers = {'Authorization':"Bearer "+ token}
+    url = "http://csr.fast.sheridanc.on.ca:50271/api/Devices/Sync"
+    
+    r = requests.post(url,headers = headers)
+
 def weightProcessing(hx):
     currentWeight = 0
     while True:
         try:
             val = hx.get_weight(5)
-            print(val , currentWeight)
+            print("{:.2f}".format(val) , "{:.2f}".format(currentWeight))
             
             if(len(buffer_values) == 5):
                 confidence = analyze(buffer_values)
                 
                 if confidence and val > currentWeight + WEIGHT_NEGLEGANCE:
-                    sendNotification("http://csr.fast.sheridanc.on.ca:50271/api/Notification","Package placed","Your package has arrived!",DEVICE_TOKEN)
+                    sendNotification("http://csr.fast.sheridanc.on.ca:50271/api/Devices/PackagePlaced")
                     currentWeight = sum(buffer_values) / len(buffer_values)
                 elif confidence and val < currentWeight - WEIGHT_NEGLEGANCE:
                     #alarmCheck()
-                    sendNotification("http://csr.fast.sheridanc.on.ca:50271/api/Notification","Package picked up","Your package has been picked up!",DEVICE_TOKEN)
+                    sendNotification("http://csr.fast.sheridanc.on.ca:50271/api/Devices/PackagePickedUp")
                     currentWeight = val
                 hx.power_down()
                 hx.power_up()
@@ -74,7 +96,7 @@ def weightProcessing(hx):
                     
             else:
                 buffer_values.append(val)
-                print(len(buffer_values))
+                #print(len(buffer_values))
 
         except (KeyboardInterrupt, SystemExit):
             cleanAndExit()
@@ -83,8 +105,9 @@ def weightProcessing(hx):
 hx = HX711(dout, pd_sck)
 hx.set_reading_format("MSB", "MSB")
 hx.set_reference_unit(REFERENCE_UNIT)
-#hx.reset()
-#hx.tare()
+hx.reset()
+hx.tare()
+sync()
 
 print("Tare done! Add weight now...")
 weightProcessing(hx)
